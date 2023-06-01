7bbbf25c3ac273ff212647bb98340e37ce622abb
Anmol Bansal <anmolbansal1807@gmail.com>
2023-06-02 00:11:59 +0530

    init

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -14,7 +14,7 @@ export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
-        <Icons.logo className="h-6 w-6" />
+        <Icons.logo className="h-8 w-8" />
         <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
       {items?.length ? (
