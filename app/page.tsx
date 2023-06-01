cedca5241e821180b0bcb5602eb06ff2a88d82b2
Anmol Bansal <anmolbansal1807@gmail.com>
2023-06-02 03:40:41 +0530

    finished landing page with zod form

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,42 @@
+"use client"
+
 import Link from "next/link"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { MessageCircle } from "lucide-react"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
 
 import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
+import { Button, buttonVariants } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { Icons } from "@/components/icons"
 
 export default function IndexPage() {
+  const formSchema = z.object({
+    characterName: z.string().min(3, { message: "Character Name is required" }),
+    characterAppearance: z.string().optional(),
+    location: z.string().min(3, { message: "Location is required" }),
+    situation: z.string().optional(),
+  })
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
     <section className="container grid place-content-center items-center gap-6 pb-8 pt-6 md:py-10">
       <Icons.logo className="h-32 w-32 place-self-center rounded-lg border-2 border-solid border-gray-500 shadow-2xl" />
@@ -16,11 +46,8 @@ export default function IndexPage() {
           <br className="hidden sm:inline" />
           of Historical Characters
         </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          {/* //text goes here */}
-        </p>
       </div>
-      <div className="flex max-w-[980px] items-center gap-2">
+      <div className="flex mt-7 max-w-[980px] items-center gap-2">
         <div className="flex flex-col items-center gap-4">
           <h4 className="text-center text-base">
             Chat with Randomly Generated
@@ -32,8 +59,67 @@ export default function IndexPage() {
             <MessageCircle className="ms-1 inline-block h-5 w-5" />
           </Link>
         </div>
-        <Separator orientation="vertical" className="m-4 h-40" />
-        
+        <Separator orientation="vertical" className="mx-8 h-52" />
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-cols-2 gap-x-4 space-y-8">
+          <h4 className="col-span-2 text-center text-base">
+            Generate your own character
+            </h4>
+            <FormField
+              control={form.control}
+              name="characterName"
+              render={({ field }) => (
+                <FormItem className="mt-8">
+                  <FormLabel>Character name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Mahatma Gandhi" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="characterAppearance"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Character Appearance</FormLabel>
+                  <FormControl>
+                    <Input placeholder="iconic round spectacles" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>location</FormLabel>
+                  <FormControl>
+                    <Input placeholder="sabarmati ashram, India" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="situation"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Situation</FormLabel>
+                  <FormControl>
+                    <Input placeholder="spinning the spinning wheel" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="col-span-2 max-w-sm place-self-center" type="submit">Generate Character</Button>
+          </form>
+        </Form>
       </div>
     </section>
   )
--- config/site.ts
+++ config/site.ts
@@ -3,12 +3,9 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   name: "Zaubar",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Discover the Lives of Historical Characters.",
   mainNav: [
-    {
-      title: "chat",
-      href: "/chat",
-    },
+    {},
   ],
   links: {
     github: "https://github.com/AnmolBansalDEV/zaubar",
